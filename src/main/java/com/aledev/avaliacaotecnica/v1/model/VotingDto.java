df1d6c3b0ce0759fb6478d675231dc7eef803e4a
Alessandro <alessandro.souza@edu.pucrs.br>
2023-02-05 19:13:23 -0300

    ajustes finais

---
+++ src/main/java/com/aledev/avaliacaotecnica/v1/model/VotingDto.java
@@ -0,0 +1,23 @@
+package com.aledev.avaliacaotecnica.v1.model;
+
+
+import com.aledev.avaliacaotecnica.v1.entity.Staff;
+import lombok.*;
+
+import java.io.Serializable;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class VotingDto implements Serializable {
+	private static final long serialVersionUID = -6641295645471857940L;
+	
+	private Staff staff;
+	private Integer totalVoteYes;
+	private Integer totalVoteNo;
+	private Integer totalVotes;
+	private Integer totalSessions;
+}
--- src/test/java/com/aledev/avaliacaotecnica/AvaliacaoTecnicaApplicationTests.java
+++ src/test/java/com/aledev/avaliacaotecnica/AvaliacaoTecnicaApplicationTests.java
@@ -1,13 +1,16 @@
 package com.aledev.avaliacaotecnica;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class AvaliacaoTecnicaApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
